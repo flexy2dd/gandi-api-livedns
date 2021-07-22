681057c25fc6287e66955285b64e1f4f9ddd2f6a
Richard Pochot <flexy2dd@blackdev.net>
2021-07-22 12:06:31 +0200

    Update to version 0.2.0

--- README.md
+++ README.md
@@ -1,1 +1,37 @@
-# gandi-api-live-dns
+<h1>Gandi.net live DNS API Client Python Library</h1>
+
+[![PyPI version](https://badge.fury.io/py/gandi-api-livedns.svg)](https://badge.fury.io/py/gandi-api-livedns)
+
+<p>Easily search for trips!</p>
+
+<h3>Installation</h3>
+<p>Works on 3.x python.</p>
+
+```
+pip install gandi-api-livedns
+```
+
+<h3>Usage</h3>
+
+```python
+from gandi_api_livedns import GandiApiLiveDNS
+
+# initialize API
+gandiApiLiveDNS = GandiApiLiveDNS(api_key="__your_api_key_here__")
+
+# set rrname and domain
+gandiApiLiveDNS.rrname = 'dummy'
+gandiApiLiveDNS.domain = 'mydomain.com'
+
+# get real ip
+RealIP = gandiApiLiveDNS.getRealIP()
+pprint.pprint(RealIP)
+
+# get registered dns record
+DNSRecord = gandiApiLiveDNS.getDNSRecord()
+pprint.pprint(DNSRecord)
+
+# update dns record with detected ip
+updateDNSRecord = gandiApiLiveDNS.updateDNSRecord()
+pprint.pprint(updateDNSRecord)
+```
--- src/gandi_api_livedns/gandi.py
+++ src/gandi_api_livedns/gandi.py
@@ -1,9 +1,6 @@
 """Provides the Gandi.net live DNS access API."""
-import asyncio
-import aiohttp
 import logging
-from aiohttp.hdrs import AUTHORIZATION
-import async_timeout
+import requests
 
 from .const import (
     DEFAULT_TIMEOUT,
@@ -20,7 +17,7 @@ class GandiApiLiveDNS:
 
     def __init__(self, 
                  domain = '', 
-                 apikey = '', 
+                 api_key = '', 
                  rrname = '', 
                  rrtype = DEFAULT_TYPE, 
                  rrttl = DEFAULT_TTL, 
@@ -35,14 +32,14 @@ class GandiApiLiveDNS:
             self._logger = logger
 
         self.domain = domain
-        self.apikey = apikey
+        self.apikey = api_key
         self.rrname = rrname
         self.rrtype = rrtype
         self.rrttl = rrttl
         self.ipv6 = ipv6
         self.timeout = timeout
 
-    async def getRealIP(self):
+    def getRealIP(self):
         """Get real external IP."""
 
         self._logger.debug("Get real ip...")
@@ -55,25 +52,21 @@ class GandiApiLiveDNS:
         self._logger.debug("Get real ip from %s", url)
 
         try:
-            with async_timeout.timeout(self.timeout):
-                async with aiohttp.ClientSession() as session:    
-                    resp = await session.get(url)
-                    body = await resp.text()
-                    self._logger.debug("Real IP: %s - %s", resp.status, body)
-                    if resp.status == 200:
-                        return body
-                    else:
-                        return False
+            resp = requests.get(url, timeout=self.timeout)
 
-        except aiohttp.ClientError:
+            self._logger.debug("Real IP: %s - %s", resp.status_code, resp.raw)
+            if resp.status_code == 200:
+                return resp.text
+
+        except requests.ConnectionError:
             self._logger.warning("Can't connect for getting real ip")
 
-        except asyncio.TimeoutError:
+        except requests.Timeout:
             self._logger.warning("Timeout from real ip getting")
 
         return False
 
-    async def getDNSRecord(self):
+    def getDNSRecord(self):
         """Get the rrset_values entry in Gandi.net API."""
 
         error = None
@@ -86,53 +79,51 @@ class GandiApiLiveDNS:
         }
 
         url = GANDI_LIVEDNS_API_URL.format(**url_params)
-        headers = {AUTHORIZATION: f"Apikey {self.apikey}"}
+        headers = {'Authorization': f"Apikey {self.apikey}"}
 
         self._logger.debug("Request url: %s", url)
 
-        try:
-            with async_timeout.timeout(self.timeout):
-                async with aiohttp.ClientSession() as session:
-                    resp = await session.get(url, headers=headers)
-                    body = await resp.json()
+        try:            
+            resp = requests.get(url, headers=headers, timeout=self.timeout)
 
-                    self._logger.debug("Getting %s: (%s) %s", url, resp.status, body)
-                    
-                    if resp.status == 200:
-                        record = body["rrset_values"][0]
-                    elif resp.status == 404:
-                        self._logger.debug("Not found")
-                        error = "record_notfound"
-                    else:
-                        self._logger.debug(
-                            "Getting %s failed: (%s) %s", url, resp.status, body
-                        )
-                        error = "cannot_connect"
+            self._logger.debug("Getting %s: (%s) %s", url, resp.status_code, resp.raw)
 
-        except aiohttp.ClientError:
-            error = "cannot_connect"
+            if resp.status_code == 200:
+                jsonResponse = resp.json()
+                record = jsonResponse['rrset_values'][0]
+            elif resp.status_code == 404:
+                self._logger.debug("Not found")
+                error = "record_notfound"
+            else:
+                self._logger.debug(
+                    "Getting %s failed: (%s) %s", url, resp.status_code, resp.raw
+                )
+                error = "cannot_connect"
+
+        except requests.ConnectionError:
+            error = "cannot_connect1"
             self._logger.warning("Can't connect to API")
 
-        except asyncio.TimeoutError:
+        except requests.Timeout:
             error = "timeout_connect"
             self._logger.warning("Timeout from API for: %s", url)
 
         return record, error
 
-    async def updateDNSRecord(self):
+    def updateDNSRecord(self):
         """Update the rrset_values and rrset_ttl entry in Gandi.net API."""
 
         error = None
         record = None
 
         self._logger.debug("Getting current real ip")
-        current_ip = await self.getRealIP()
+        current_ip = self.getRealIP()
         if not current_ip:
             self._logger.warning("Can't get the real ip")
             return record, "cannot_get_realip"
 
         self._logger.debug("Getting current DNS record ip")
-        current_gandi_ip, error = await self.getDNSRecord()
+        current_gandi_ip, error = self.getDNSRecord()
         if not current_gandi_ip:
             self._logger.warning("Can't get the current dns ip")
             return record, error
@@ -150,31 +141,27 @@ class GandiApiLiveDNS:
 
         url = GANDI_LIVEDNS_API_URL.format(**url_params)
         json = {"rrset_ttl": self.rrttl, "rrset_values": [current_ip]}
-        headers = {AUTHORIZATION: f"Apikey {self.apikey}"}
+        headers = {'Authorization': f"Apikey {self.apikey}"}
 
         self._logger.debug("Update DNS record")
         try:
-            with async_timeout.timeout(self.timeout):
-                async with aiohttp.ClientSession() as session:
-                    resp = await session.put(url, json=json, headers=headers)
-                    body = await resp.text()
-                    
-                    if resp.status == 201:
-                        self._logger.info(
-                            "Record updated with ttl: %s ip: %s", self.rrttl, current_ip
-                        )
-                        record = current_ip
-                    else:
-                        self._logger.warning(
-                            "Updating %s failed: (%s) %s", url, resp.status, body
-                        )
-                        error = "update_failed"
+            resp = requests.put(url, headers=headers, json=json, timeout=self.timeout)
+            if resp.status_code == 201:
+                self._logger.info(
+                    "Record updated with ttl: %s ip: %s", self.rrttl, current_ip
+                )
+                record = current_ip
+            else:
+                self._logger.warning(
+                    "Updating %s failed: (%s) %s", url, resp.status, body
+                )
+                error = "update_failed"
 
-        except aiohttp.ClientError:
+        except requests.ConnectionError:
             error = "cannot_connect"
             self._logger.warning("Can't connect to API")
 
-        except asyncio.TimeoutError:
+        except requests.Timeout:
             error = "timeout_connect"
             self._logger.warning("Timeout from API for: %s", self.domain)
 
